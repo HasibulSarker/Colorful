285929fe2d9b802ab6f6e017d9f36efe51b2532f
HasibulSarker <63333502+HasibulSarker@users.noreply.github.com>
2023-11-28 13:57:52 +0600

    Dynamic Contact Section

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -11,5 +11,27 @@ global.addEventListener("DOMContentLoaded", function () {
 
     positionChange.style.position = "relative";
   };
-  
+
+  var activatorButton = this.document.getElementById("contact-injector");
+
+  var injectionPoint = this.document.getElementById("contact");
+
+  activatorButton.addEventListener("click", function () {
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "Data/Contact.html");
+
+    xhr.send();
+
+    xhr.onload = function () {
+
+      let responseData = xhr.responseText;
+
+      injectionPoint.innerHTML = responseData;
+
+    };
+
+  });
+
 });
